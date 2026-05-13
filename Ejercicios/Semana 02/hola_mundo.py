e625adb664639ea17bfa698a3862cfd3e1f9fe98
CapitanDiez <1725111093@utectulancingo.edu.mx>
2026-05-13 14:49:33 +0000

    CREATED ejercicio hola mundo.py

---
+++ Ejercicios/Semana 02/hola_mundo.py
@@ -0,0 +1,10 @@
+class NombreClase:
+
+    def __init__(self):
+        print("Constructor")
+
+    def metodoUno(self):
+        print("Metodo Uno")
+
+nombre_objeto = NombreClase()
+nombre_objeto.metodoUno()
